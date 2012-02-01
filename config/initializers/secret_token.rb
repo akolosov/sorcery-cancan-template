3f5f0ac4c5c92122562c8b6dd7df2d2950ac08f6
Alexey Kolosov <alexey.kolosov@gmail.com>
2012-02-01 15:55:34 +0600

    Исправлена ошибка с Assets

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-{programName}::Application.config.secret_token = '71c3cda60b08f02cfb9db858ed020470d2e9c9bd4c4342b36672486064ba12ad208624a6c26064079c5d646e4972640dd8d0fb27a95af4c3bf74e8c73cd2e1a0'
+{programName}::Application.config.secret_token = '00f6afc847b1e55c7662a7ddfc8a8ce8a0f61248f89e36b1cf3f2c22f6222288375095cfd76d2e93a8acaa19e271d047d8c89d406c36d6abfa8b274ab2e85d6a'
